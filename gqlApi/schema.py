0871a5f377129b359d815408ed53a6413945a3a0
savan <savan.soni0312@gmail.com>
2021-01-08 11:55:26 +0530

    added parameterized query to graphql

--- gqlApi/schema.py
+++ gqlApi/schema.py
@@ -9,5 +9,9 @@ class HeroType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     heroes = graphene.List(HeroType)
+    hero = graphene.Field(HeroType, id= graphene.Int())
     def resolve_heroes(self, info, **kwargs):
         return Hero.objects.all()
+    def resolve_hero(self, info, **kwargs):
+        cur = kwargs.get('id')
+        return Hero.objects.get(pk=cur)
